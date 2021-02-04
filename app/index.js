64bd3011a8e6481c5d21d9a98479b9948c0f86a8
sjekpo <sydney.j.ekpo@gmail.com>
2021-02-04 21:37:29 +0100

    Created HTML pages for Project Explorer

--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const SERVER_PORT = 4000;
 
-app.use(express.static('public/project-explorer'));
+app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
